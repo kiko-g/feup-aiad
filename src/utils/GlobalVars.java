60cf16f320c1800d8200a762fa6b7ea21f7b0bb5
PedroBaptista3622 <ze.pedro3622@outlook.pt>
2020-11-25 20:25:20 +0000

    Removed JavaFX related code

--- src/utils/GlobalVars.java
+++ src/utils/GlobalVars.java
@@ -2,7 +2,7 @@ package utils;
 
 public class GlobalVars {
     public final static int MAX_PLAYERS = 24;
-    public final static int DISCUSSION_MAX_TIME_SECONDS = 10;
+    public final static int DISCUSSION_MAX_TIME_SECONDS = 1;
     public final static double VOTE_MIN_SUS_VALUE = 0.75;
     public static final boolean VERBOSE = false;
 }
